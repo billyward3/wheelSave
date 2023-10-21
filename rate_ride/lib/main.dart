d078daae838c926c0176cca304375e8d81e863b3
Jaydin_MacBook <JaydinFreemanWork@gmail.com>
2023-10-21 18:02:33 -0400

    Added Sensor and Geographical Data
    Added sensor, geographical data and perms

--- rate_ride/lib/main.dart
+++ rate_ride/lib/main.dart
@@ -1,16 +1,32 @@
-// Required Flutter import
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:sensors_plus/sensors_plus.dart';
 
-// Main entry point for the Flutter app
+class GeolocatorService {
+  Future<Position?> determinePosition() async {
+    LocationPermission permission;
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.deniedForever) {
+        return Future.error(
+            'Location permissions are permanently denied. Please enable them in settings.');
+      }
+    } else if (permission == LocationPermission.deniedForever) {
+      throw Exception(
+          'Location permissions are permanently denied. Please enable them in settings.');
+    }
+    return await Geolocator.getCurrentPosition();
+  }
+}
+
 void main() {
   runApp(const MyApp());
 }
 
-// Represents the overall Flutter application structure
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // Builds and returns the main app layout
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,20 +40,29 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// Represents the main screen or home page of the app
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
-  // Creates the mutable state for this widget
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-// Contains the mutable state and UI logic for the MyHomePage widget
 class _MyHomePageState extends State<MyHomePage> {
-  // The main layout builder for MyHomePage
+  late Future<Position?>
+      _locationData; // Changed to Position? since determinePosition may return null
+  Stream<AccelerometerEvent>? _accelerometerStream;
+  bool _isStarted = false;
+  final GeolocatorService _geolocatorService =
+      GeolocatorService(); // Instantiate GeolocatorService
+
+  @override
+  void initState() {
+    super.initState();
+    _accelerometerStream = accelerometerEvents;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,28 +73,68 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            // Displays placeholder text for accelerometer data
-            const Text(
-              'Accelerometer Data Goes Here',
-              style: TextStyle(color: Colors.white),
-            ),
-            // Displays placeholder text for real-time location data
-            const Text(
-              'Real-Time Location Data Goes Here',
-              style: TextStyle(color: Colors.white),
-            ),
+            _isStarted
+                ? StreamBuilder<AccelerometerEvent>(
+                    stream: _accelerometerStream,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return Text(
+                          'Accelerometer: ${snapshot.data}',
+                          style: TextStyle(color: Colors.white),
+                        );
+                      } else if (snapshot.hasError) {
+                        return Text(
+                          'Error: ${snapshot.error}',
+                          style: TextStyle(color: Colors.red),
+                        );
+                      }
+                      return const Text('Waiting for accelerometer data...');
+                    },
+                  )
+                : Container(),
+            _isStarted
+                ? FutureBuilder<Position?>(
+                    future: _locationData,
+                    builder: (BuildContext context,
+                        AsyncSnapshot<Position?> snapshot) {
+                      if (snapshot.connectionState == ConnectionState.done) {
+                        if (snapshot.hasError) {
+                          return Text(
+                            'Location Error: ${snapshot.error}',
+                            style: TextStyle(color: Colors.red),
+                          );
+                        }
+                        return Text(
+                          'Location: ${snapshot.data}',
+                          style: TextStyle(color: Colors.white),
+                        );
+                      } else {
+                        return const Text('Fetching location data...');
+                      }
+                    },
+                  )
+                : Container(),
             SizedBox(height: 20),
-            // A button which will be used to start data collection (to be implemented)
             ElevatedButton(
-              onPressed: () {
-                // Placeholder for logic to start accelerometer and location data collection
+              onPressed: () async {
+                try {
+                  Position? position =
+                      await _geolocatorService.determinePosition();
+                  setState(() {
+                    _isStarted = true;
+                    _locationData = Future.value(
+                        position); // Convert Position? into a Future<Position?>
+                  });
+                } catch (e) {
+                  ScaffoldMessenger.of(context)
+                      .showSnackBar(SnackBar(content: Text(e.toString())));
+                }
               },
               child: const Text('Start'),
             ),
           ],
         ),
       ),
-      // No floating action button, as the counter functionality has been removed
     );
   }
 }
